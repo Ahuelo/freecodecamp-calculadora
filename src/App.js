c635e02e5c501a251d274807eb221bf22fec6b69
Ivan Montenegro <nelson.moncar@outlook.com>
2022-10-03 19:08:35 -0500

    pantalla calculadora

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import freeCodeCampLogo from './img/freecodecamp-logo.png'
 import { Button } from './Button';
+import { Pantalla } from './Screen';
 
 function App() {
   return (
@@ -12,15 +13,34 @@ function App() {
         alt='Logo de FreeCodeCamp' />
       </div>
       <div className='contenedor-calculadora'>
+        <Pantalla/>
+        <div className='fila'>
+          <Button>7</Button>
+          <Button>8</Button>
+          <Button>9</Button>
+          <Button>+</Button>
+        </div>
+        <div className='fila'>
+          <Button>4</Button>
+          <Button>5</Button>
+          <Button>6</Button>
+          <Button>-</Button>
+        </div>
         <div className='fila'>
           <Button>1</Button>
-          <Button>+</Button>
-          <Button>1</Button>
+          <Button>2</Button>
+          <Button>3</Button>
+          <Button>x</Button>
         </div>
-        <div className='fila'></div>
-        <div className='fila'></div>
-        <div className='fila'></div>
-        <div className='fila'></div>
+        <div className='fila'>
+          <Button>=</Button>
+          <Button>0</Button>
+          <Button>.</Button>
+          <Button>/</Button>
+        </div>
+        <div className='fila'>
+
+        </div>
       </div>
     </div>
   );
---
+++ src/Screen/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Screen.css'
+
+const Pantalla = ({ input }) =>{
+    <div className='input'>
+        {input}
+    </div>
+}
+
+export { Pantalla };
